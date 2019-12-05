4defdb0beae1df5eedba5f8e1939d43cbd195d2c
ahmetsaiderdem <57713153+ahmetsaiderdem@users.noreply.github.com>
2019-12-06 02:47:11 +0300

    Add files via upload

--- susi.py
+++ susi.py
@@ -1,26 +1,28 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication,QMainWWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow
 
 
-class MainForm(QMainWWindow):
+class MainForm(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Calculator')
         self.setGeometry(200,500,500,500)
+        self.initUI()
         
-    def İnitUI(self):
+        
+    def initUI(self):
         self.lbl_sayi1 =QtWidgets.QLabel(self)
         self.lbl_sayi1.setText('sayi1: ')
         self.lbl_sayi1.move(50,30)
 
         self.txt_sayi1 =QtWidgets.QLineEdit(self)
         self.txt_sayi1.move(150,30)
-        self.txt_resize(200,30)
+        self.txt_sayi1.resize(200,30)
 
         self.lbl_sayi2 = QtWidgets.QLabel(self)
         self.lbl_sayi2.setText('sayi2: ')
-        self.lbl_sayi2.move(200,30)
+        self.lbl_sayi2.move(50,80)
 
         self.txt_sayi2 = QtWidgets.QLineEdit(self)
         self.txt_sayi2.move(150,80)
@@ -28,7 +30,7 @@ class MainForm(QMainWWindow):
 
         self.btn_topla=QtWidgets.QPushButton(self)
         self.btn_topla.setText('Toplama')
-        self.btn_topla.move(150,130)
+        self.btn_topla.move(150,250)
         self.btn_topla.clicked.connect(self.hesapla)
 
         self.btn_cikar=QtWidgets.QPushButton(self)
@@ -46,9 +48,9 @@ class MainForm(QMainWWindow):
         self.btn_bolme.move(150,130)
         self.btn_bolme.clicked.connect(self.hesapla)
 
-        self.lbl_sonuc = QtWidgets.Qlabe(self)
+        self.lbl_sonuc = QtWidgets.QLabel(self)
         self.lbl_sonuc.setText('sonuc : ')
-        self.lbl_sonuc.move(150,200)
+        self.lbl_sonuc.move(100,300)
 
     def hesapla(self):
         sender = self.sender().text()
@@ -57,18 +59,18 @@ class MainForm(QMainWWindow):
         if sender == 'Toplama':
             result = int(self.txt_sayi1.text())+int(self.txt_sayi2.text())
         elif sender == 'çıkarma':
-            result = int(self.txt_sayi1.text())+int(self.txt_sayi2.text())
+            result = int(self.txt_sayi1.text())-int(self.txt_sayi2.text())
         elif sender == 'Çarpma':
-            result = int(self.txt_sayi1.text())+int(self.txt_sayi2.text())    
+            result = int(self.txt_sayi1.text())*int(self.txt_sayi2.text())    
         elif sender == 'Bölme':
-            result = int(self.txt_sayi1.text())+int(self.txt_sayi2.text())
+            result = int(self.txt_sayi1.text())/int(self.txt_sayi2.text())
         
-        self.txt_sonuc.setText('sonuc : ' + str(result))
+        self.lbl_sonuc.setText('sonuc : ' + str(result))
 
 
 def app():
     app = QtWidgets.QApplication(sys.argv)
-    win=MainForm()
+    win = MainForm()
     win.show()
     sys.exit(app.exec_())
 
